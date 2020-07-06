155bfe79512284b704a708c4bba4382e3b951ecf
Danielthweatt <dthweatt192@yahoo.com>
2020-07-06 13:33:08 -0500

    building views - partials (header and footer), login page (and corresponding route)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,21 @@
 const debug = require('debug')('app:userController');
 
 module.exports = {
-    home(req, res) {
-        debug('home()');
+    getLoginForm(req, res) {
+        debug('getLoginForm()');
+
+        debug('Rendering login form view...');
+        res.render('login-form', { title: 'Login' });
+    },
+    getHome(req, res) {
+        debug('getHome()');
+
+        const user = { name: 'John Smith' };
 
         debug('Rendering home view...');
         res.render('home', { 
-            mainHeading: 'Budget App' 
+            title: 'Home',
+            user
         });
     }
 };
--- routes/main/index.js
+++ routes/main/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userController = require('../../controllers/userController');
 
-router.get('/', userController.home);
+router.get('/', userController.getHome);
+router.get('/login', userController.getLoginForm);
 
 module.exports = router;
