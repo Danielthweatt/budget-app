81adef82b4471a4d578b5e713860f37370556f2d
Danielthweatt <dthweatt192@yahoo.com>
2020-08-03 10:27:22 -0500

    building out tests for routes

--- tests/integration/routes.test.js
+++ tests/integration/routes.test.js
@@ -2,17 +2,18 @@ const request = require('supertest');
 const { User } = require('../../models');
 
 let server;
+const testAccount = {
+    username: 'Test Account', 
+    email: 'test@example.com', 
+    password: 'Testing1!'
+};
 
-const createTestAccount = async () => {
-    const testAccount = {
-        username: 'Test Account', 
-        email: 'test@example.com', 
-        password: 'Testing1!'
-    };
+const createUser = async (username, email, password) => {
+    const user = { username, email, password };
 
-    await (new User(testAccount)).save();
+    await (new User(user)).save();
 
-    return testAccount;
+    return user;
 };
 
 describe('routes', () => {
@@ -43,7 +44,9 @@ describe('routes', () => {
         });
 
         it('should return a redirect if the user is logged in', async () => {
-            const { username, password } = await createTestAccount();
+            const { username, email, password } = testAccount;
+
+            await createUser(username, email, password);
 
             const agent = request.agent(server);
 
@@ -57,8 +60,10 @@ describe('routes', () => {
     });
 
     describe('POST /sign-up', () => {
-        it('should not create a user if a user is already logged in', async () => {
-            let { username, password } = await createTestAccount();
+        it('should not create a user and return a redirect if a user is already logged in', async () => {
+            let { username, email, password } = testAccount;
+            
+            await createUser(username, email, password);
 
             const agent = request.agent(server);
 
@@ -66,9 +71,9 @@ describe('routes', () => {
 
             username = 'Test Account 2';
 
-            await agent.post('/sign-up').send({
+            const res = await agent.post('/sign-up').send({
                 username,
-                email: 'test2@example.com',
+                email,
                 password,
                 confirmPassword: password
             });
@@ -76,17 +81,194 @@ describe('routes', () => {
             const user = await User.findOne({ username });
 
             expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
         });
 
-        it('should return a redirect if the user is logged in', async () => {
-            const { username, password } = await createTestAccount();
+        it('should not create a user and return a redirect if the username is less than 3 characters', async () => {
+            let { username, email, password } = testAccount;
+            
+            username = new Array(3).join('a');
 
-            const agent = request.agent(server);
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
 
-            await agent.post('/login').send({ username, password });
+            const user = await User.findOne({ username });
 
-            const res = await agent.post('/sign-up');
+            expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
 
+        it('should not create a user and return a redirect if the username is more than 50 characters', async () => {
+            let { username, email, password } = testAccount;
+            
+            username = new Array(52).join('a');
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const user = await User.findOne({ username });
+
+            expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
+
+        it('should not create a user and return a redirect if the email is invalid', async () => {
+            let { username, email, password } = testAccount;
+            
+            email = 'a';
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const user = await User.findOne({ username });
+
+            expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
+
+        it('should not create a user and return a redirect if the password is less than 8 characters', async () => {
+            let { username, email, password } = testAccount;
+            
+            password = new Array(8).join('a');
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const user = await User.findOne({ username });
+
+            expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
+
+        it('should not create a user and return a redirect if the password is more than 50 characters', async () => {
+            let { username, email, password } = testAccount;
+            
+            password = new Array(52).join('a');
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const user = await User.findOne({ username });
+
+            expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
+
+        it('should not create a user and return a redirect if the password has no lowercase letters', async () => {
+            let { username, email, password } = testAccount;
+            
+            password = `${new Array(8).join('A')}1`;
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const user = await User.findOne({ username });
+
+            expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
+
+        it('should not create a user and return a redirect if the password has no uppercase letters', async () => {
+            let { username, email, password } = testAccount;
+            
+            password = `${new Array(8).join('a')}1`;
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const user = await User.findOne({ username });
+
+            expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
+
+        it('should not create a user and return a redirect if the password has no numbers', async () => {
+            let { username, email, password } = testAccount;
+            
+            password = `A${new Array(8).join('a')}`;
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const user = await User.findOne({ username });
+
+            expect(user).toBeNull();
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
+
+        it('should not create a user and return a redirect if a user by that username already exists', async () => {
+            const { username, email, password } = testAccount;
+            
+            await createUser(username, email, password);
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const users = await User.find({ username });
+
+            expect(users.length).toBe(1);
+            expect(res.status).toBe(302);
+            expect(res.redirect).toBeTruthy();
+        });
+
+        it('should create a user and return a redirect if no user is logged in, the input is valid, and a user by that username does not exist', async () => {
+            const { username, email, password } = testAccount;
+
+            const res = await request(server).post('/sign-up').send({
+                username,
+                email,
+                password,
+                confirmPassword: password
+            });
+
+            const user = await User.findOne({ username });
+
+            expect(user).not.toBeNull();
             expect(res.status).toBe(302);
             expect(res.redirect).toBeTruthy();
         });
