2e2c56c626c9704d65201952acae1b56dd0ed1f9
Danielthweatt <dthweatt192@yahoo.com>
2020-08-27 16:51:06 -0500

    building global modal scripts and modal for creating a new budget

--- public/js/globals.js
+++ public/js/globals.js
@@ -1,11 +1,25 @@
 //Scripts for every page
-//Toggle display of navbar menu on touch devices
-//https://bulma.io/documentation/components/navbar#navbarJsExample
 $(document).ready(function() {
+    //Toggle display of navbar menu on touch devices
+    //https://bulma.io/documentation/components/navbar#navbarJsExample
     //Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
+    $('.navbar-burger').click(function() {
         //Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
+        $('.navbar-burger').toggleClass('is-active');
+        $('.navbar-menu').toggleClass('is-active');
+    });
+
+    //Open modal targeted by data attribute on any element with .open-modal class
+    $('.open-modal').click(function() {
+        var targetModal = $(this).data('target-modal');
+
+        $('#' + targetModal).addClass('is-active');
+    });
+
+    //Close modal targeted by data attribute on any element with .close-modal class
+    $('.close-modal').click(function() {
+        var targetModal = $(this).data('target-modal');
+
+        $('#' + targetModal).removeClass('is-active');
     });
 });
